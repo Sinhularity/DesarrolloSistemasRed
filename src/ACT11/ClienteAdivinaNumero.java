89279300f4072de305c7dc44e19b716a6b8b196f
Sinhularity <zs22017021@estudiantes.uv.mx>
2025-03-27 22:42:54 -0600

    Update ACT11

--- src/ACT11/ClienteAdivinaNumero.java
+++ src/ACT11/ClienteAdivinaNumero.java
@@ -1,9 +1,15 @@
 package ACT11;
 
+import ACT10.ManejadorSocket;
+
 import javax.swing.*;
+import java.io.EOFException;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ClienteAdivinaNumero extends JFrame{
     private JTextField Prompt;
@@ -12,23 +18,23 @@ public class ClienteAdivinaNumero extends JFrame{
     private JScrollPane ResultadosScroll;
     private JPanel MainFrame;
 
-    ObjectOutputStream salida;
-    ObjectInputStream entrada;
+
+    private ObjectOutputStream salida;
+    private ObjectInputStream entrada;
     private int numberAttempt;
 
     public void initComponents ( ) {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(MainFrame);
         this.setResizable(false);
-        this.setVisible(true);
 
         ResultadosScroll.setViewportView(Resultados);
         Resultados.setEditable(false);
 
-
         enviarButton.addActionListener(e -> {
             getAttempt();
             sendAttempt(numberAttempt);
+
         });
 
         this.pack();
@@ -38,9 +44,42 @@ public class ClienteAdivinaNumero extends JFrame{
     public void getAttempt () {
         if (!Prompt.getText().isEmpty()) {
             numberAttempt = Integer.parseInt(Prompt.getText());
+            System.out.println("Intento: " + numberAttempt);
         } else {
             numberAttempt = 1;
         }
+    }
+
+    public void sendAttempt (int num) {
+        try {
+            System.out.println("Enviando intento al servidor: " + num);
+            salida.writeObject(num);
+            salida.flush();
+        } catch (Exception e) {
+            Logger.getLogger(ClienteAdivinaNumero.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+    void receiveMessage () {
+        new Thread( () -> {
+            try {
+                while (true) {
+                    Object object = entrada.readObject();
+                    if (object != null) {
+                        if (object.equals("¡Correcto!")) {
+                            Resultados.append(object + "\n");
+                            enviarButton.setEnabled(false);
+                            Prompt.setEnabled(false);
+                        } else {
+                            Prompt.setText("");
+                            Resultados.append(object + "\n");
+                        }
+                    }
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                Logger.getLogger(ClienteAdivinaNumero.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }).start();
     }
 
     public ClienteAdivinaNumero () {
@@ -48,34 +87,14 @@ public class ClienteAdivinaNumero extends JFrame{
         try {
             Socket socket = new Socket("localhost", 777);
             System.out.println("Conexión establecida con el servidor.");
+
             salida = new ObjectOutputStream(socket.getOutputStream());
             entrada = new ObjectInputStream(socket.getInputStream());
 
-            while (true) {
-                try {
-                    Object object = entrada.readObject();
-                    Resultados.append(object + "\n");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
+           receiveMessage();
         } catch (Exception e) {
-            e.printStackTrace();
+            Logger.getLogger(ClienteAdivinaNumero.class.getName()).log(Level.SEVERE, null, e);
         }
-    }
-
-    public void sendAttempt (int num) {
-        try {
-            salida.writeInt(num);
-            salida.flush();
-        } catch (Exception e) {
-            System.out.println("Error al enviar el número al servidor.");
-        }
-    }
-
-    public void getServerResponse () {
-
     }
 
     public static void main(String[] args) {
--- src/ACT11/ServidorAdivinaNumero.java
+++ src/ACT11/ServidorAdivinaNumero.java
@@ -12,7 +12,6 @@ import java.util.logging.Logger;
 public class ServidorAdivinaNumero {
 
     private ServerSocket servidor;
-    private Vector<ClienteAdivinaNumero> clienteAdivinaNumeros;
     private int numeroAdivinar;
     private ObjectInputStream entrada;
     private ObjectOutputStream salida;
@@ -20,25 +19,26 @@ public class ServidorAdivinaNumero {
     public ServidorAdivinaNumero () {
         try {
             servidor = new ServerSocket(777);
-            System.out.println("Servidor iniciado en el puerto 777");
             numeroAdivinar = (int) (Math.random() * 100) + 1;
-            System.out.println("Número secreto generado: " + numeroAdivinar);
             Socket socket = servidor.accept();
-            System.out.println("Conexión establecida con el cliente: " + socket.getInetAddress());
+            System.out.println("Servidor iniciado en el puerto 777");
+            System.out.println("Número secreto generado: " + numeroAdivinar);
+            entrada = new ObjectInputStream(socket.getInputStream());
+            salida = new ObjectOutputStream(socket.getOutputStream());
 
             while (true) {
                 try {
-
-                    int ClientAttemptNumber = entrada.readInt();
-                    getClientAttempt(ClientAttemptNumber);
-
+                    Object ClientAttemptNumber = entrada.readObject();
+                    System.out.println("Intento del cliente: " + ClientAttemptNumber);
+                    if ((ClientAttemptNumber != null)) {
+                        getClientAttempt((Integer) ClientAttemptNumber);
+                    }
                 } catch (IOException e) {
-                    System.out.println("Error al leer el número del cliente.");
-
+                    Logger.getLogger(ServidorAdivinaNumero.class.getName()).log(Level.SEVERE, null, e);
+                    break;
                 }
-
             }
-
+            socket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -49,14 +49,12 @@ public class ServidorAdivinaNumero {
         try {
             if (num == numeroAdivinar) {
                 salida.writeObject("¡Correcto!");
-                salida.flush();
             } else if (numeroAdivinar > num) {
-                salida.writeObject("Muy alto");
-                salida.flush();
-            } else {
                 salida.writeObject("Muy bajo");
-                salida.flush();
+            } else {
+                salida.writeObject("Muy alto");
             }
+            salida.flush();
         } catch (IOException e) {
             System.out.println("Error al enviar el mensaje al cliente.");
         }
